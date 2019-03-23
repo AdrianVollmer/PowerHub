2666522f66090823c507d938a787e3c0d31cdb89
Adrian Vollmer <adrian.vollmer@syss.de>
2019-03-23 12:23:04 +0100

    add basic webdav functionality

---
+++ powerhub/webdav.py
@@ -0,0 +1,25 @@
+from cheroot import wsgi
+from wsgidav.wsgidav_app import WsgiDAVApp
+
+config = {
+    "host": "0.0.0.0",
+    "port": 8001,
+    "provider_mapping": {
+        "/": "webdav",
+        },
+    "verbose": 1,
+    }
+
+app = WsgiDAVApp(config)
+
+server_args = {
+    "bind_addr": (config["host"], config["port"]),
+    "wsgi_app": app,
+    }
+server = wsgi.Server(**server_args)
+try:
+    server.start()
+except KeyboardInterrupt:
+    print("Caught Ctrl-C, shutting down...")
+finally:
+    server.stop()
