a64aa08e0e5fef1b5d062cfb05dd7da19b9932fa
Adrian Vollmer <adrian.vollmer@syss.de>
2019-03-23 21:24:33 +0100

    enable anonymous webdav auth; use watchdog to get blackhole functionality

--- powerhub/webdav.py
+++ powerhub/webdav.py
@@ -1,15 +1,42 @@
+import os
+import threading
 from cheroot import wsgi
 from wsgidav.wsgidav_app import WsgiDAVApp
-from powerhub.directories import WEBDAV_DIR
+from powerhub.directories import WEBDAV_PUBLIC, WEBDAV_RO, WEBDAV_BLACKHOLE, \
+        BLACKHOLE_DIR
 from powerhub.args import args
-
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 config = {
     "host": args.LHOST,
     "port": args.WEBDAV_PORT,
+    "dir_browser": {"enable": True},
+    "http_authenticator": {
+        # None: dc.simple_dc.SimpleDomainController(user_mapping)
+        "domain_controller": None,
+        "accept_basic": True,  # Allow basic authentication, True or False
+        "accept_digest": True,  # Allow digest authentication, True or False
+        "default_to_digest": True,  # True  or False
+        # Name of a header field that will be accepted as authorized user
+        "trusted_auth_header": None,
+    },
+    #: Used by SimpleDomainController only
+    "simple_dc": {"user_mapping": {"*": True}},
     "provider_mapping": {
-        "/": WEBDAV_DIR,
+        "/ro": {
+            "root": WEBDAV_RO,
+            "readonly": True,
+            "auth": "anonymous",
         },
+        "/public": {
+            "root": WEBDAV_PUBLIC,
+        },
+        "/blackhole": {
+            "root": WEBDAV_BLACKHOLE,
+        }
+    },
     "verbose": 1,
     }
 
@@ -22,7 +49,33 @@ server_args = {
 server = wsgi.Server(**server_args)
 
 
+class MyHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        os.rename(
+            os.path.join(event.src_path),
+            os.path.join(BLACKHOLE_DIR, os.path.basename(event.src_path)),
+        )
+
+
+def watch_blackhole_folder():
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=WEBDAV_BLACKHOLE, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
 def run_webdav():
+    threading.Thread(
+        target=watch_blackhole_folder,
+        daemon=True,
+    ).start()
     try:
         server.start()
     except KeyboardInterrupt:
