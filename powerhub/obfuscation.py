410539ba25ad4d9ea7c029fa67b29570144fe120
Adrian Vollmer <adrian.vollmer@syss.de>
2019-03-28 17:53:16 +0100

    add a comment and TODO

--- powerhub/obfuscation.py
+++ powerhub/obfuscation.py
@@ -13,6 +13,10 @@ def symbol_name(name):
 
 
 def choose_obfuscated_name():
+    # TODO choose better names
+    # the names should not contain too much entropy, or it will be
+    # detectable. they should also not be too common or we will risk
+    # collisions. they should just blend in perfectly.
     result = None
     while not result and result in list(symbol_list.values()):
         result = ''.join([random.choice(string.ascii_lowercase)
