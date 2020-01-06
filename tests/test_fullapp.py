5de21b2c8904b61751e2065b07dc010933246e50
Adrian Vollmer <adrian.vollmer@syss.de>
2020-01-06 20:31:22 +0100

    add more tests: pth; lshm

--- tests/test_fullapp.py
+++ tests/test_fullapp.py
@@ -14,9 +14,10 @@ import pytest
 
 # https://stackoverflow.com/a/33515264/1308830
 sys.path.append(os.path.join(os.path.dirname(__file__), 'helpers'))
-from test_init import TEST_URI, TEST_COMMANDS, init_tests, \
-        NEW_XDG_DATA_HOME  # noqa
+from test_init import TEST_URI, TEST_COMMANDS, init_tests  # noqa
 
+
+MAX_TEST_MODULE_PS1 = 103
 
 init_tests()
 
@@ -83,12 +84,10 @@ def execute_cradle(cmd):
 
 def create_modules():
     func = "function Invoke-Testfunc%(n)d { Write-Host 'Test%(n)d' }"
-    for i in range(102):
+    from powerhub.directories import MOD_DIR
+    for i in range(MAX_TEST_MODULE_PS1):
         with open(
-            os.path.join(NEW_XDG_DATA_HOME,
-                         "modules",
-                         "ps1",
-                         "psmod%d.ps1" % i),
+            os.path.join(MOD_DIR, "ps1", "psmod%d.ps1" % i),
             "w"
         ) as f:
             f.write(func % {"n": i})
@@ -120,18 +119,48 @@ def test_stager(full_app):
     )
 
     win10cmd = TEST_COMMANDS["win10"] % full_app
-    out = execute_cradle(win10cmd)
+    # Insert formatter for extra command
+    win10cmd = win10cmd[:-2] + '%s' + win10cmd[-2:]
+    out = execute_cradle(win10cmd % "")
     assert "Adrian Vollmer" in out
     assert "Run 'Help-PowerHub' for help" in out
 
-    out = execute_cradle(win10cmd + ";lshm")
-    assert "psmod53" in out
+    out = execute_cradle(win10cmd % "lshm")
+    for i in range(MAX_TEST_MODULE_PS1):
+        assert "psmod%d" % i in out
 
-    out = execute_cradle(win10cmd + ";lhm psmod53;Invoke-Testfunc53")
+    out = execute_cradle(win10cmd % "lhm psmod53;Invoke-Testfunc53")
     expected = """Test53
 Name            Type N  Loaded
 ----            ---- -  ------
-ps1/psmod53.ps1 ps1  71   True"""
+ps1/psmod53.ps1 ps1  72   True"""
     assert "Test53" in out
     assert "psmod53" in out
     assert expected in out.replace('\r\n', '\n')
+
+    out = execute_cradle(
+        win10cmd % ('lhm \\"72-74,77\\";lshm;Invoke-Testfunc53;'
+                    + "Invoke-Testfunc99;Invoke-Testfunc47;Invoke-Testfunc72;")
+    )
+    # I don't understand the order of the modules
+    assert "Test53" in out
+    assert "Test99" in out
+    assert "Test47" in out
+    assert "Test72" in out
+
+    from powerhub.directories import UPLOAD_DIR
+    testfile = "testfile.dat"
+    out = execute_cradle(
+        win10cmd % ('[io.file]::WriteAllBytes(\\"$env:TEMP/%s\\",(1..255));' %
+                    testfile + 'pth \\"$env:TEMP/%s\\";' % testfile)
+    )
+    with open(os.path.join(UPLOAD_DIR, testfile), "rb") as f:
+        data = f.read()
+    assert data == bytes(range(1, 256))
+
+    out = execute_cradle(
+        win10cmd % ('\\"FooBar123\\"|pth -name %s;' % testfile)
+    )
+    with open(os.path.join(UPLOAD_DIR, testfile+".1"), "rb") as f:
+        data = f.read()
+    assert data == b"FooBar123"
