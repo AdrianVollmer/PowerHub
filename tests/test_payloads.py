172a8f1fbc17f02bdd1f91d77157cf53d66ac6f2
Adrian Vollmer <adrian.vollmer@syss.de>
2020-05-21 10:27:57 +0200

    Add dotnet payload test

--- tests/test_payloads.py
+++ tests/test_payloads.py
@@ -16,3 +16,22 @@ def test_gcc():
     assert filename == 'powerhub-mingw32-64bit-reflection-http.exe'
     assert b"DOS" in payload
     assert payload.startswith(b'MZ')
+
+
+def test_mcs():
+    from powerhub.payloads import create_dotnet
+    args = {
+        "GroupLauncher": "dotnetexe-64bit",
+        "GroupAmsi": "reflection",
+        "GroupTransport": "http",
+        "GroupClipExec": "none",
+        "CheckboxProxy": "false",
+        "CheckboxTLS1.2": "false",
+        "RadioFingerprint": "true",
+        "RadioNoVerification": "false",
+        "RadioCertStore": "false",
+    }
+    filename, payload = create_dotnet(args)
+    assert filename == 'powerhub-dotnetexe-64bit-reflection-http.exe'
+    assert b"DOS" in payload
+    assert payload.startswith(b'MZ')
