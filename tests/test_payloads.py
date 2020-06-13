a56eefc0905799421350f3994912fd8b0eceafc3
Adrian Vollmer <adrian.vollmer@syss.de>
2020-06-13 19:00:13 +0200

    Implement test case for vbs payload

--- tests/test_payloads.py
+++ tests/test_payloads.py
@@ -1,8 +1,12 @@
+from test_init import execute_cmd
 
-def test_gcc():
-    from powerhub.payloads import create_exe
+import pytest
+
+
+@pytest.fixture
+def get_args():
     args = {
-        "GroupLauncher": "mingw32-64bit",
+        "GroupLauncher": None,
         "GroupAmsi": "reflection",
         "GroupTransport": "http",
         "GroupClipExec": "none",
@@ -12,25 +16,42 @@ def test_gcc():
         "RadioNoVerification": "false",
         "RadioCertStore": "false",
     }
+    yield args
+
+
+def test_vbs(get_args):
+    from powerhub.payloads import create_vbs
+    args = get_args
+    args['GroupLauncher'] = 'vbs'
+    filename, payload = create_vbs(args)
+    assert filename == 'powerhub-vbs-reflection-http.vbs'
+
+    import tempfile
+    tmpf = tempfile.NamedTemporaryFile('w', delete=False)
+    tmpf.write(payload)
+    tmpf.close()
+
+    execute_cmd("ssh win10 del C:/Windows/Temp/powerhub.vbs")
+    execute_cmd("scp %s win10:C:/Windows/Temp/powerhub.vbs" % tmpf.name)
+    out = execute_cmd("ssh win10 cscript.exe C:/Windows/Temp/powerhub.vbs")
+    assert "Windows Script Host" in out
+    assert "error" not in out
+
+
+def test_gcc(get_args):
+    from powerhub.payloads import create_exe
+    args = get_args
+    args['GroupLauncher'] = 'mingw32-64bit'
     filename, payload = create_exe(args)
     assert filename == 'powerhub-mingw32-64bit-reflection-http.exe'
     assert b"DOS" in payload
     assert payload.startswith(b'MZ')
 
 
-def test_mcs():
+def test_mcs(get_args):
     from powerhub.payloads import create_dotnet
-    args = {
-        "GroupLauncher": "dotnetexe-64bit",
-        "GroupAmsi": "reflection",
-        "GroupTransport": "http",
-        "GroupClipExec": "none",
-        "CheckboxProxy": "false",
-        "CheckboxTLS1.2": "false",
-        "RadioFingerprint": "true",
-        "RadioNoVerification": "false",
-        "RadioCertStore": "false",
-    }
+    args = get_args
+    args['GroupLauncher'] = 'dotnetexe-64bit'
     filename, payload = create_dotnet(args)
     assert filename == 'powerhub-dotnetexe-64bit-reflection-http.exe'
     assert b"DOS" in payload
