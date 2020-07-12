0ab232adf382370cb55207c7f4a539cb06d49f93
Adrian Vollmer <adrian.vollmer@syss.de>
2020-07-12 12:47:13 +0200

    Add test executing the paylods

--- tests/test_payloads.py
+++ tests/test_payloads.py
@@ -19,6 +19,27 @@ def get_args():
     yield args
 
 
+def copy_and_execute(filename, payload, interpreter=""):
+    import tempfile
+    import subprocess
+    if isinstance(payload, str):
+        tmpf = tempfile.NamedTemporaryFile('w', delete=False)
+    else:
+        tmpf = tempfile.NamedTemporaryFile('wb', delete=False)
+    tmpf.write(payload)
+    tmpf.close()
+
+    try:
+        execute_cmd(f"ssh win10 del C:/Windows/Temp/{filename}")
+    except subprocess.CalledProcessError:
+        # this happens if the file does not exist
+        pass
+
+    execute_cmd(f"scp {tmpf.name} win10:C:/Windows/Temp/{filename}")
+    out = execute_cmd(f"ssh win10 {interpreter} C:/Windows/Temp/{filename}")
+    return out
+
+
 def test_vbs(get_args):
     from powerhub.payloads import create_vbs
     args = get_args
@@ -26,20 +47,7 @@ def test_vbs(get_args):
     filename, payload = create_vbs(args)
     assert filename == 'powerhub-vbs-reflection-http.vbs'
 
-    import tempfile
-    import subprocess
-    tmpf = tempfile.NamedTemporaryFile('w', delete=False)
-    tmpf.write(payload)
-    tmpf.close()
-
-    try:
-        execute_cmd("ssh win10 del C:/Windows/Temp/powerhub.vbs")
-    except subprocess.CalledProcessError:
-        # this happens if the file does not exist
-        pass
-
-    execute_cmd("scp %s win10:C:/Windows/Temp/powerhub.vbs" % tmpf.name)
-    out = execute_cmd("ssh win10 cscript.exe C:/Windows/Temp/powerhub.vbs")
+    out = copy_and_execute(filename, payload, "cscript.exe")
     assert "Windows Script Host" in out
     assert "error" not in out
 
@@ -53,6 +61,9 @@ def test_gcc(get_args):
     assert b"DOS" in payload
     assert payload.startswith(b'MZ')
 
+    out = copy_and_execute(filename, payload)
+    assert out == ""
+
 
 def test_mcs(get_args):
     from powerhub.payloads import create_dotnet
@@ -62,3 +73,7 @@ def test_mcs(get_args):
     assert filename == 'powerhub-dotnetexe-64bit-reflection-http.exe'
     assert b"DOS" in payload
     assert payload.startswith(b'MZ')
+
+    out = copy_and_execute(filename, payload)
+
+    assert out == ""
