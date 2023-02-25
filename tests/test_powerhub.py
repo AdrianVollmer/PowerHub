b43dfb9f79b7258240739e2580dd2d8205d189ba
Adrian Vollmer <adrian.vollmer@syss.de>
2023-02-25 19:09:30 +0100

    Update all the tests

--- tests/test_powerhub.py
+++ tests/test_powerhub.py
@@ -6,7 +6,6 @@ import tempfile
 import sys
 import re
 
-import flask
 import pytest
 
 # https://stackoverflow.com/a/33515264/1308830
@@ -18,56 +17,60 @@ init_tests()
 
 
 @pytest.fixture(scope="module")
-def flask_app():
+def test_client():
     temp_db = tempfile.mkstemp()[1]
+
     from powerhub.app import PowerHubApp
-    app = PowerHubApp([TEST_URI, '--no-auth'])
-    from powerhub.flask import app as blueprint
-    flask_app = flask.Flask(__name__, static_url_path='/invalid', template_folder="../powerhub/templates")
-    flask_app.register_blueprint(blueprint)
-    flask_app = flask_app.test_client()
-    yield flask_app
+    from powerhub.args import parse_args
+    args = parse_args([TEST_URI, '--no-auth'])
+    app = PowerHubApp(args)
+
+    yield app.flask_app.test_client()
+
     os.remove(temp_db)
     app.stop()
 
 
-def test_initial_redirection(flask_app):
-    response = flask_app.get('/')
+def test_initial_redirection(test_client):
+    response = test_client.get('/', headers={'Accept': 'text/html'})
     assert b"Redirecting..." in response.data
     assert b'<a href="/hub' in response.data
 
 
-def test_hub_page(flask_app):
-    response = flask_app.get('/hub')
+def test_hub_page(test_client):
+    response = test_client.get('/hub', headers={'Accept': 'text/html'})
     assert b"PowerHub" in response.data
     assert b"Hub" in response.data
     assert b"This is the Hub" in response.data
     assert b"AMSI Bypass" in response.data
 
 
-def test_clipboard_page(flask_app):
-    response = flask_app.get('/clipboard')
+def test_clipboard_page(test_client):
+    response = test_client.get('/clipboard', headers={'Accept': 'text/html'})
     assert b"PowerHub" in response.data
     assert b"Clipboard" in response.data
     assert b"Clipboard is empty" in response.data
 
 
-def test_clipboard_add_del(flask_app):
+def test_clipboard_add_del(test_client):
     try:
         from cgi import escape
     except ImportError:
         from html import escape
     reps = 8
     content = '<strong>NO HTML</strong> allowed here'
-    response = flask_app.get('/clipboard').data.decode()
+    response = test_client.get('/clipboard', headers={'Accept': 'text/html'}).data.decode()
     entry_count = response.count('card-body')
     test_count = response.count(escape(content))
     for i in range(reps):
-        response = flask_app.post('/clipboard/add', data={
-            'content': content,
-        }, follow_redirects=False)
+        response = test_client.post(
+            '/clipboard/add',
+            data={'content': content},
+            headers={'Accept': 'text/html'},
+            follow_redirects=False,
+        )
         assert response.status == '302 FOUND'
-    response = flask_app.get('/clipboard').data.decode()
+    response = test_client.get('/clipboard', headers={'Accept': 'text/html'}).data.decode()
     assert "Delete all" in response
     assert "Export" in response
     new_entry_count = response.count('card-body')
@@ -79,34 +82,39 @@ def test_clipboard_add_del(flask_app):
     # delete
     for i in range(reps):
         e_id = re.findall(r'data-id="([0-9]+)"', response)[-1]
-        response = flask_app.post('/clipboard/delete', data={
-            'id': e_id,
-        }, follow_redirects=False)
-        response = flask_app.get('/clipboard').data.decode()
+        response = test_client.post(
+            '/clipboard/delete',
+            headers={'Accept': 'text/html'},
+            data={
+                'id': e_id,
+            },
+            follow_redirects=False,
+        )
+        response = test_client.get('/clipboard').data.decode()
         assert ('data-id="%s"' % e_id) not in response
 
 
-def test_fileexchange_page(flask_app):
-    from powerhub.directories import UPLOAD_DIR
-    f = tempfile.NamedTemporaryFile("w+", dir=UPLOAD_DIR)
+def test_fileexchange_page(test_client):
+    from powerhub.directories import directories
+    f = tempfile.NamedTemporaryFile("w+", dir=directories.UPLOAD_DIR)
     size = 617
     f.write("0"*size)
     f.flush()
-    response = flask_app.get('/fileexchange').data.decode()
+    response = test_client.get('/fileexchange', headers={'Accept': 'text/html'}).data.decode()
     assert "PowerHub" in response
     assert "File Exchange" in response
     assert os.path.basename(f.name) in response
     assert "<td>%d</td>" % size in response
 
 
-def test_static(flask_app):
-    from powerhub.directories import STATIC_DIR
-    f = tempfile.NamedTemporaryFile("w+", dir=STATIC_DIR)
+def test_static(test_client):
+    from powerhub.directories import directories
+    f = tempfile.NamedTemporaryFile("w+", dir=directories.STATIC_DIR)
     basename = os.path.basename(f.name)
     size = 617
     f.write("0"*size)
     f.flush()
-    response = flask_app.get('/static/'+basename).data.decode()
+    response = test_client.get('/static/'+basename, headers={'Accept': 'text/html'}).data.decode()
     assert "0"*size == response
 
 #  TODO:
