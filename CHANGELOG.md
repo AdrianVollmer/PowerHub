dad2f6fdbaa68f6b315e76701a6cd448ca6232b1
Adrian Vollmer <adrian.vollmer@syss.de>
2023-03-12 17:29:22 +0100

    Version bump: 2.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+## [2.0.2] - 2023-03-12
+
 ### Changed
 
 * Update info hints in the Hub tab
--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2018-2023, Adrian Vollmer'
 author = 'Adrian Vollmer'
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.1"
+release = "2.0.2"
 
 
 # -- General configuration ---------------------------------------------------
