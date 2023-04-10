b29e420d8385c6fece598f111ee608710c07a357
Adrian Vollmer <computerfluesterer@protonmail.com>
2023-04-10 16:42:01 +0200

    Some more obfuscation (block size)

--- powerhub/templates/powershell/rc4.ps1
+++ powerhub/templates/powershell/rc4.ps1
@@ -6,21 +6,23 @@ function {{symbol_name("xor")}} {
 
 {#- Text book implementation of RC4 -#}
 
+${{symbol_name('block_size')}} = 256;
+
 function {{symbol_name("Decrypt-RC4")}} {
     param (${{symbol_name('data')}}, ${{symbol_name('key')}});
-    ${{symbol_name('s')}} = New-Object Byte[] 256;
-    ${{symbol_name('k')}} = New-Object Byte[] 256;
+    ${{symbol_name('s')}} = New-Object Byte[] ${{symbol_name('block_size')}};
+    ${{symbol_name('k')}} = New-Object Byte[] ${{symbol_name('block_size')}};
 
-    for (${{symbol_name('i')}} = 0; ${{symbol_name('i')}} -lt 256; ${{symbol_name('i')}}++)
+    for (${{symbol_name('i')}} = 0; ${{symbol_name('i')}} -lt ${{symbol_name('block_size')}}; ${{symbol_name('i')}}++)
     {
         ${{symbol_name('s')}}[${{symbol_name('i')}}] = ${{symbol_name('i')}};
         ${{symbol_name('k')}}[${{symbol_name('i')}}] = ${{symbol_name('key')}}[${{symbol_name('i')}} % ${{symbol_name('key')}}.Length];
     };
 
     ${{symbol_name('j')}} = 0;
-    for (${{symbol_name('i')}} = 0; ${{symbol_name('i')}} -lt 256; ${{symbol_name('i')}}++)
+    for (${{symbol_name('i')}} = 0; ${{symbol_name('i')}} -lt ${{symbol_name('block_size')}}; ${{symbol_name('i')}}++)
     {
-        ${{symbol_name('j')}} = (${{symbol_name('j')}} + ${{symbol_name('s')}}[${{symbol_name('i')}}] + ${{symbol_name('k')}}[${{symbol_name('i')}}]) % 256;
+        ${{symbol_name('j')}} = (${{symbol_name('j')}} + ${{symbol_name('s')}}[${{symbol_name('i')}}] + ${{symbol_name('k')}}[${{symbol_name('i')}}]) % ${{symbol_name('block_size')}};
         ${{symbol_name('m')}} = ${{symbol_name('s')}}[${{symbol_name('i')}}];
         ${{symbol_name('s')}}[${{symbol_name('i')}}] = ${{symbol_name('s')}}[${{symbol_name('j')}}];
         ${{symbol_name('s')}}[${{symbol_name('j')}}] = ${{symbol_name('m')}};
@@ -29,12 +31,12 @@ function {{symbol_name("Decrypt-RC4")}} {
     ${{symbol_name('i')}} = ${{symbol_name('j')}} = 0;
     for (${{symbol_name('x')}} = 0; ${{symbol_name('x')}} -lt ${{symbol_name('data')}}.Length; ${{symbol_name('x')}}++)
     {
-        ${{symbol_name('i')}} = (${{symbol_name('i')}} + 1) % 256;
-        ${{symbol_name('j')}} = (${{symbol_name('j')}} + ${{symbol_name('s')}}[${{symbol_name('i')}}]) % 256;
+        ${{symbol_name('i')}} = (${{symbol_name('i')}} + 1) % ${{symbol_name('block_size')}};
+        ${{symbol_name('j')}} = (${{symbol_name('j')}} + ${{symbol_name('s')}}[${{symbol_name('i')}}]) % ${{symbol_name('block_size')}};
         ${{symbol_name('m')}} = ${{symbol_name('s')}}[${{symbol_name('i')}}];
         ${{symbol_name('s')}}[${{symbol_name('i')}}] = ${{symbol_name('s')}}[${{symbol_name('j')}}];
         ${{symbol_name('s')}}[${{symbol_name('j')}}] = ${{symbol_name('m')}};
-        [int]${{symbol_name('t')}} = (${{symbol_name('s')}}[${{symbol_name('i')}}] + ${{symbol_name('s')}}[${{symbol_name('j')}}]) % 256;
+        [int]${{symbol_name('t')}} = (${{symbol_name('s')}}[${{symbol_name('i')}}] + ${{symbol_name('s')}}[${{symbol_name('j')}}]) % ${{symbol_name('block_size')}};
         ${{symbol_name('data')}}[${{symbol_name('x')}}] = {{symbol_name("xor")}} ${{symbol_name('data')}}[${{symbol_name('x')}}] ${{symbol_name('s')}}[${{symbol_name('t')}}];
     };
 
